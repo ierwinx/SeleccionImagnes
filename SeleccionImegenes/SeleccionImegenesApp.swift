7d356b4f1250357570146f2ac818ac89ae2a5297
Eяw!и Gontier <ierwinx@me.com>
2022-10-10 13:16:27 -0500

    Initial Commit

---
+++ SeleccionImegenes/SeleccionImegenesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SeleccionImegenesApp.swift
+//  SeleccionImegenes
+//
+//  Created by Erwin Luz León on 10/10/22.
+//
+
+import SwiftUI
+
+@main
+struct SeleccionImegenesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
